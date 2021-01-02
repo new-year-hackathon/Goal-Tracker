4fa32c66800a9ac726a26ca3619a63823db85152
Catalina McQuade <catalinamcquade@gmail.com>
2021-01-02 11:40:07 -0500

    feat: initialized react components and redux store

---
+++ client/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import {Navbar, Header} from './components';
+// import Routes from './routes';
+
+const App = () => {
+  return (
+    <div>
+      {/* <Header />
+      <Navbar />
+      <Routes /> */}
+    </div>
+  );
+};
+
+export default App;

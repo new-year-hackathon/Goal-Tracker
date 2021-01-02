c6f3017b7927fec6fdf5d43a83ed4f3db20c0bfe
Catalina McQuade <catalinamcquade@gmail.com>
2021-01-02 17:42:20 -0500

    feat: started models for user, goals, and pets, and started seed file

---
+++ server/db/goal.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+const db = require('./database');
+
+const Goal = db.define('goal', {
+
+})
+
+module.exports = Goal;
---
+++ server/db/pet.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+const db = require('./database');
+
+const Pet = db.define('pet', {
+
+})
+
+module.exports = Pet;

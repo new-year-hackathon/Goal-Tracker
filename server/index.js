6d57b114dd27e7fbd15963f4af975f46abeab78c
Catalina McQuade <catalinamcquade@gmail.com>
2021-01-01 23:34:02 -0500

    feat: set up localhost server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-// where the port will go
+'use strict'
+
+const express = require('express')
+const {db} = require('./db')
+const app = express()
+const PORT = process.env.PORT || 1337
+
+db.sync()
+  .then(() => {
+    console.log('db synced')
+    app.listen(PORT, () => console.log(`studiously serving silly sounds on port ${PORT}`))
+  })

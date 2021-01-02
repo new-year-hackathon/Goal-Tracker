3524cfad33cc4c7acff1407683d037cca94f1537
Catalina McQuade <catalinamcquade@gmail.com>
2021-01-02 16:04:56 -0500

    update: trying to turn Home React component into Homepage (needs fixing)

---
+++ client/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Route, BrowserRouter as Router, Link } from "react-router-dom"
+import Home from "./Home"
+import Goals from "./Goals"
+import Shop from "./Shop"
+
+const Navbar = () => {
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <Link to="/home">Home</Link> 
+                    <Link to="/goals">My Goals</Link>
+                    <Link to="/shop">Shop</Link>
+                </nav>
+                <Route exact path="/home" component={Home} />
+                <Route exact path="/goals" component={Goals} />
+                <Route exact path="/shop" component={Shop} />
+            </div>
+        </Router>
+    );
+};
+  
+export default Navbar;
+  
